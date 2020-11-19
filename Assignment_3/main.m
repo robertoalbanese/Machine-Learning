15ad2d43f6a84c6e9525f496a6cdd751a2c596ed
Robbai <ralbanese18@gmail.com>
2020-11-19 10:40:36 +0100

    minor changes

--- Assignment_3/main.m
+++ Assignment_3/main.m
@@ -3,24 +3,37 @@ clc;
 clear all;
 close all;
 %% Task1: Obtain a data set
-[train_set, train_lb] = loadMNIST('train-images.idx3-ubyte');
-[test_set, test_lb] = loadMNIST('t10k-images.idx3-ubyte');
+[train_set, train_lb] = loadMNIST(0);
+[test_set, test_lb] = loadMNIST(1);
 %% Task2: Build k-NN classifier
-k = 3;
+k = [1:3];
 labels = unique(train_lb);
 % [classification, error] = kNNclassifier(train_set,train_lb,test_set,k,test_lb);
 classification = [];
 err = [];
-for j = 1 :size(labels,1)
-        tmpTrainlabel = train_lb;
-        tmpTrainlabel(tmpTrainlabel ~= labels(j)) = 0;
-        %tmpTrain_set = [train_set tmpTrainlabel];
-        
-        tmpTest = test_lb;
-        tmpTest(tmpTest ~= labels(j)) = 0;
-        tmpTest_set = [test_set tmpTest];
-        for i =  1 : max(size(k))
-                    [classification(:,j, i), err(i)] = kNNclassifier(train_set, test_set,tmpTrainlabel, k(i), tmpTest);
-        end
 
+tmp = [test_set test_lb];
+tmp = randSet(tmp,15/size(test_set,1));
+
+h = waitbar(0,'Please wait...');
+
+for j = 1 :size(labels,1)
+    
+    tmpTrainlabel = train_lb;
+    tmpTrainlabel(tmpTrainlabel ~= labels(j)) = 0;
+    
+    tmpTestLabel = tmp(:,end);
+    tmpTestLabel(tmpTestLabel ~= labels(j)) = 0;
+    tmpTest_set = tmp(:,1:end-1);
+    for i = 1:size(k,2)
+        [classification(:,j,i), err(j,i)] = kNNclassifier(train_set, tmpTrainlabel, tmpTest_set, k(i), tmpTestLabel);
+    end
+ 
+    waitbar(j/size(labels,1))
+    
 end
+
+for i = 1:size(k,2)
+    averageError(i) = sum(err(:,i))/size(err,1);
+end
+close(h)
