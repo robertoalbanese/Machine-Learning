25077f719f06ec8af9f7acfca307efa64b5d60bf
Robbai <ralbanese18@gmail.com>
2021-10-08 16:36:39 +0200

    Final commit

--- Assignment_4/main.m
+++ Assignment_4/main.m
@@ -28,19 +28,19 @@ eta1 = 2; %used for Perceptron algorithm
 
 k1 = n;
 k2 = 2;
-k3 = 6;
+k3 = 20;
 
 %% Learning phase and Confusion matrix computation for iris/MNIST dataset
 cond = 0;
 [train_set, test_set] = partition(dataset,k3,cond);
-% [c_mat_per,iterations] = perceptron(train_set,test_set, eta1,labels);
+%[c_mat_per,iterations] = perceptron(train_set,test_set, eta1,labels);
 [c_mat_ad, iterations] = adaline(train_set, test_set, eta2,labels,cond);
 [accuracy,err_freq,sensitivity,specificity,F_measure]  = qualityIndices(c_mat_ad,n);
 
 %% Learning phase and Confusion matrix computation for xor dataset
 cond = 1;
 [train_set_xor, test_set_xor] = partition(dataset_xor,k2,cond);
-% [c_mat_per_xor, iterations] = perceptron(train_set_xor,test_set_xor, eta1,labels_xor);
-[c_mat_ad_xor, iterations] = adaline(train_set_xor, test_set_xor, eta2,labels_xor,cond);
+[c_mat_per_xor, iterations] = perceptron(train_set_xor,test_set_xor, eta1,labels_xor);
+%[c_mat_ad_xor, iterations] = adaline(train_set_xor, test_set_xor, eta2,labels_xor,cond);
 
-[accuracy_xor,err_freq_xor,sensitivity_xor,specificity_xor,F_measure_xor]  = qualityIndices(c_mat_ad_xor,n_xor);
+[accuracy_xor,err_freq_xor,sensitivity_xor,specificity_xor,F_measure_xor]  = qualityIndices(c_mat_per_xor,n_xor);
--- Assignment_5/main.m
+++ Assignment_5/main.m
@@ -11,11 +11,11 @@ labels = (unique(train_labels));
 % Training Parameters
 trials = 2;
 hl = 1;
-% Initialize vectors of pattern
+% Initialize 2 vectors for storing labels values.
 X = zeros(1,trials);
 Y = zeros(1,trials);
-for i = 1 : trials
-    while (X(i) == Y(i))
+for i = 1 : trials 
+    while (X(i) == Y(i))% N.B. The 2 vectors must not have same values 
         X(i) = randi(numel(labels));
         Y(i) = randi(numel(labels));
     end
@@ -24,12 +24,12 @@ end
 figure
 for j = 1 : trials
     subplot(floor(trials/2), 2, j)
-    % Create a training set with only 2 classes
-    train_trio = train_data(train_labels(:,1) == X(j) | train_labels(:,1) == Y(j), :);
+    % Create a training set with only 2 DIFFERENT classes
+    train_subset = train_data(train_labels(:,1) == X(j) | train_labels(:,1) == Y(j), :);
     % Train an autoencoder on the new, reduced training set
-    myAutoencoder = trainAutoencoder(train_trio', hl);
+    myAutoencoder = trainAutoencoder(train_subset', hl);
     % Encode the different classes using the encoder obtained
-    myEncodedData = encode(myAutoencoder, train_trio');
+    myEncodedData = encode(myAutoencoder, train_subset');
     % Plot data using plotcl
     subplot(2,2,j), hold on
     plotcl(myEncodedData', train_labels(train_labels(:,1) == X(j) | train_labels(:,1) == Y(j)));
